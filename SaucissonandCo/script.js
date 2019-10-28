feaae544ebdb4d6a48adae4539c6f9c9babcfa48
johnnyl25 <lemesle.johnny@live.fr>
2019-10-28 16:38:00 +0100

    Saucisson and Co

---
+++ SaucissonandCo/script.js
@@ -0,0 +1,31 @@
+// navigation éléments navbar
+
+$(function () {
+
+    $(".navbar a").on("click", function (e) {
+
+        e.preventDefault();
+        let hash = this.hash;
+
+        $('html').animate({ scrollTop: $(hash).offset().top }, 800, function () { window.location.hash = hash; });
+
+    });
+
+})
+
+// bouton retour en haut
+
+mybutton = document.getElementById("myBtn");
+
+window.addEventListener("scroll", function(){
+  if (document.documentElement.scrollTop > 200) {
+    mybutton.style.bottom = "30px";
+
+  } else {
+    mybutton.style.bottom = "-100px";
+  }
+});
+
+function topFunction() {
+    $("html").animate({ scrollTop: "0" }, "1500");
+}
